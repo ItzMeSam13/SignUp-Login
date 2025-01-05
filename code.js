82e47386955c96e111bb7ff644678137cbab3b03
ItzMeSam <sampreetkalkundrikar8@gmail.com>
2025-01-05 17:07:40 +0530

    modified v7

--- code.js
+++ code.js
@@ -1,5 +1,7 @@
 let sup = document.querySelector(".btn1");
 let Lgn = document.querySelector(".btn2");
+let supchoice = document.querySelector(".signup-choice");
+
 sup.addEventListener("click",()=>{
-    console.log("hello");
+    
 });
