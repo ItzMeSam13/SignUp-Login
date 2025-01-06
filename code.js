9edde14ee248135652e8375e1c41b3caa1a828ea
ItzMeSam <sampreetkalkundrikar8@gmail.com>
2025-01-06 22:31:44 +0530

    completed with signup section and started with login section v10

--- code.js
+++ code.js
@@ -5,27 +5,54 @@ const opt = document.querySelector(".choice");
 const sup_form = document.querySelector(".signup-choice");
 const container = document.querySelector(".signup-login");
 const head = document.querySelector("h3");
+const inp = document.querySelectorAll("input");
+const Lgn_form=document.querySelector(".login-choice");
 sup_form.style.display = "none";
+Lgn_form.style.display="none";
 sup.addEventListener("click",()=>{
+    Lgn_form.style.display = "none";
     head.innerText="Sign up";
     opt.style.display = "none";
     sup_form.style.display = "block";
     // Trigger the stretching animation
-    container.classList.add("expanded");
+    container.classList.add("expanded1");
     // Wait for the animation to complete, then show the form
     setTimeout(() => {
         opt.style.display = "none";
         sup_form.style.display = "block";
-    }, 500); // Match the CSS transition duration (0.5s)
+    }, 1000); // Match the CSS transition duration (1.5s)
 });
-back.addEventListener("click",()=>{
+back.addEventListener("click",(ev)=>{
+    ev.preventDefault();
+    for(let i of inp)
+    {
+        if(i.value!="")
+            i.value="";
+    }
     sup_form.style.display = "none";
     opt.style.display = "block";
     head.innerText="Sign up Or Login";
-    container.classList.remove("expanded");
+    container.classList.remove("expanded1");
     // Wait for the shrinking animation, then show the options
     setTimeout(() => {
         opt.style.display = "block";
         head.innerText = "Sign up Or Login";
-    }, 500); // Match the CSS transition duration (0.5s
+    }, 1000); // Match the CSS transition duration (1.5s)
 });
+Lgn.addEventListener("click",()=>{
+    container.classList.add("expanded2");
+    setTimeout(() => {
+        opt.style.display = "none";
+        sup_form.style.display ="none"
+        Lgn_form.style.display = "block";
+    }, 10);
+    head.innerText="Login with your email";
+    
+
+});
+
+/*Lgn_form.style.display = "block";
+    // Trigger the stretching animation
+    container.classList.add("expanded");
+    // Wait for the animation to complete, then show the form
+    // Match the CSS transition duration (1.5s) */
